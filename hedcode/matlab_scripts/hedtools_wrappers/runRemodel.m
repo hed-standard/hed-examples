0eb8d0237696c71a314b8d29138c344e3895b6bc
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2023-02-20 09:10:00 -0600

    Added MATLAB wrappers for remodeling tools

--- hedcode/matlab_scripts/hedtools_wrappers/runRemodel.m
+++ hedcode/matlab_scripts/hedtools_wrappers/runRemodel.m
@@ -1,14 +1,9 @@
-function issueString = runRemodelBackup(dataPath, backup_args)
-% Validate the HED annotations in a BIDS dataset
+function runRemodel(remodel_args)
+% Run the remodeling tools.
 % 
 % Parameters:
-%    dataPath  - Full path to the root directory of a BIDS dataset.
-%
-% Returns:
-%     issueString - A string with the validation issues suitable for
-%                   printing (has newlines).
+%    remodel_args  - Full path to the root directory of a BIDS dataset.
 %
     py.importlib.import_module('hed');
-    bids = py.hed.tools.BidsDataset(dataPath);
-    issues = bids.validate();
-    issueString = string(py.hed.get_printable_issue_string(issues));
+    py.hed.tools.remodeling.cli.run_remodel.main(remodel_args);
+  
