f53023413974fb6f5e3e48f8d413622e9f0ffa84
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2023-08-28 11:25:05 -0500

    Removed the python.version from the readthedocs.yaml

--- hedcode/matlab_scripts/web_services/runTest.m
+++ hedcode/matlab_scripts/web_services/runTest.m
@@ -2,5 +2,6 @@
 % host = 'https://hedtools.ucsd.edu/hed';
 host = 'http://127.0.0.1:5000/';
 %host = 'https://hedtools.ucsd.edu/hed_dev';
-errors = testLibraryServices(host);
+%errors = testLibraryServices(host);
 %errors = testSpreadsheetServices(host);
+errors = testStringServices(host);
